0f5d9d660e2a7ea2ce879f6dabc1756509e1f7a8
Moritz <yeet@Moritzs-MacBook-Air.fritz.box>
2025-10-31 17:27:06 +0100

    Spawn 10 spheres on Startup

--- src/components.rs
+++ src/components.rs
@@ -2,3 +2,12 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Asteroid;
+
+#[derive(Component)]
+pub struct Ant;
+
+#[derive(Component)]
+pub struct AntHive;
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,20 @@ use bevy::{color::palettes::tailwind, prelude::*};
 
 use crate::bevy_camera::*;
 use crate::components::*;
+use crate::setup_world::*;
 
 mod bevy_camera;
 mod components;
+mod setup_world;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
-        .add_plugins(YeetCameraPlugin);
-    app.add_systems(Startup, (setup_world, setup_player, setup_lights));
+        .add_plugins(YeetCameraPlugin)
+        .add_plugins(YeetSetupWorldPlugin);
+    app.add_systems(Startup, setup_player);
 
     app.run();
-}
-
-fn setup_world(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let sphere_mesh = meshes.add(Sphere::new(2.0));
-    let sphere_material = materials.add(Color::from(tailwind::BLUE_700));
-    commands.spawn((
-        Mesh3d(sphere_mesh),
-        MeshMaterial3d(sphere_material.clone()),
-        Transform::from_xyz(0.0, 0.0, -10.0),
-    ));
 }
 
 fn setup_player(mut commands: Commands) {
@@ -34,21 +23,5 @@ fn setup_player(mut commands: Commands) {
         Camera3d::default(),
         Player,
         Transform::from_xyz(0.0, 0.0, 0.0),
-    ));
-}
-
-fn setup_lights(mut commands: Commands) {
-    commands.spawn(AmbientLight {
-        color: Color::from(tailwind::SKY_950),
-        ..default()
-    });
-
-    commands.spawn((
-        PointLight {
-            color: Color::from(tailwind::YELLOW_100),
-            intensity: 40_000_000.0, // this value seems to create a good brightness of the pointlight
-            ..default()
-        },
-        Transform::from_xyz(5.0, 10.0, 5.0),
     ));
 }
